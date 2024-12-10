9c9d9e3979c157f77e373791a6bb0e8bbffa7b71
Alan Campora <alan.campora@gmail.com>
2024-12-10 12:59:05 -0300

    ignore unnecessary files

--- server/src/scripts/migrate-to-mongo.ts
+++ server/src/scripts/migrate-to-mongo.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import { Tree } from '../models/tree';
-import data from '../../data/output_street_0.json';
+import data from '../../data/output_park_1.json';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -13,17 +13,52 @@ async function run() {
     await mongoose.connect(connectionString);
 
     // Transformar datos a formato GeoJSON para 'location'
-    const docs = data.map(item => ({
-      ...item,
-      type: "street",
-      location: {
-        type: "Point",
-        coordinates: [
-          parseFloat(item.long),
-          parseFloat(item.lat)
-        ]
+    const docs = data.map((item: any) => {
+      const type = "park";
+      let street_info;
+      let park_info;
+      if (type == "street") {
+        street_info = {
+          district: item.district,
+          street_name: item.street_name,
+          street_number: item.street_number,
+          street_metal_number: item.street_metal_number,
+          sidewalk_width: item.sidewalk_width,
+          planter_condition: item.planter_condition,
+          planter_location: item.planter_location,
+          planter_level: item.planter_level,
+
+        }
       }
-    }));
+
+
+      if (type == "park") {
+        park_info = {
+          park: item.park,
+          inclination: item.inclination,
+          common_name: item.common_name,
+          leaf_type: item.leaf_type,
+          family_type: item.family_type,
+          genus_name: item.genus_name,
+          origin: item.origin,
+          coord_x: item.coord_x,
+          coord_y: item.coord_y,
+        }
+      }
+      return {
+        ...item,
+        type,
+        park_info,
+        street_info,
+        location: {
+          type: "Point",
+          coordinates: [
+            parseFloat(item.long),
+            parseFloat(item.lat)
+          ]
+        }
+      }
+    });
 
     const result = await Tree.insertMany(docs);
     console.log(`Se insertaron ${result.length} documentos.`);
