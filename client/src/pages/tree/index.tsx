d19c4a92fdfb073562cccb07894e5e8df652ac88
Alan Campora <alan.campora@gmail.com>
2024-12-14 11:56:41 -0300

    client: add skeleton

--- client/src/pages/tree/index.tsx
+++ client/src/pages/tree/index.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../components/header";
 import { Tree } from "@/lib/types";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const TreeProfilePage = () => {
   const { id } = useParams(); // Get the ID from the URL
   const [tree, setTree] = useState<Tree>(); // State for tree data
   const [error, setError] = useState(null); // State for error handling
+  const [isLoading, setIsLoading] = useState(true); // State for error handling
 
   useEffect(() => {
     const fetchTreeData = async () => {
@@ -17,8 +19,10 @@ const TreeProfilePage = () => {
         }
         const data = await response.json();
         setTree(data);
+        setIsLoading(false);
       } catch (err: any) {
         setError(err.message);
+        setIsLoading(false);
       }
     };
 
@@ -36,27 +40,40 @@ const TreeProfilePage = () => {
         <div className="">
           <div className="bg-pattern w-full"></div>
           <div className="relative flex justify-center m-4">
-            <img
+            {isLoading && <Skeleton className="w-32 h-32 rounded-full" />}
+            {!isLoading && <img
               src={tree?.information?.img}
               alt={tree?.scientific_name}
               className="w-32 h-32 rounded-full border-4 border-white shadow-lg"
             />
+            }
           </div>
         </div>
 
         {/* Profile Info */}
-        <div className="text-center mt-4">
+        {isLoading && <div className="space-y-2 mt-4 flex items-center flex-col">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[250px]" />
+        </div>
+        }
+        {!isLoading && <div className="text-center mt-4">
           <h1 className="text-2xl font-bold">{tree?.information?.name}</h1>
           <p className="text-gray-600">{tree?.scientific_name}</p>
         </div>
+        }
+
+        {isLoading && <Skeleton className="max-w-3xl mx-auto mt-8 shadow rounded-lg p-6" />}
 
         {/* Tree Details */}
-        <div className="max-w-3xl mx-auto mt-8 bg-white shadow rounded-lg p-6">
+        {!isLoading && <div className="max-w-3xl mx-auto mt-8 bg-white shadow rounded-lg p-6">
           <h2 className="text-xl font-semibold text-gray-800">Descripcion</h2>
           <p className="text-justify">{tree?.information?.summary}</p>
         </div>
+        }
 
-        <div className="max-w-3xl mx-auto mt-8 bg-white shadow rounded-lg p-6">
+
+        {isLoading && <Skeleton className="max-w-3xl mx-auto mt-8 shadow rounded-lg p-6" />}
+        {!isLoading && <div className="max-w-3xl mx-auto mt-8 bg-white shadow rounded-lg p-6">
           <h2 className="text-xl font-semibold text-gray-800">Detalles</h2>
           <ul className="mt-4 space-y-2">
             <li
@@ -80,6 +97,7 @@ const TreeProfilePage = () => {
             </li>
           </ul>
         </div>
+        }
 
         {/* Optional Footer */}
         <footer className="mt-8 text-center text-gray-500">
