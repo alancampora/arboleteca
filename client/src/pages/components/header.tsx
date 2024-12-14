fd6120f6b713da851e3c6f15074b1b6c66caef56
Alan Campora <alan.campora@gmail.com>
2024-12-14 16:16:31 -0300

    (fe): new header

--- client/src/pages/components/header.tsx
+++ client/src/pages/components/header.tsx
@@ -1,10 +1,19 @@
 export default function Header() {
   return (
-    <header className="bg-green-600 text-white text-center py-8">
-      <h1 className="text-4xl font-bold">🌳 Arboleteca</h1>
-      <p className="text-xl mt-2">Descubre los secretos de los árboles que te rodean</p>
-    </header>
+    <>
+      {
+        false &&
+        <header className="bg-green-600 text-white text-center py-8">
+          <h1 className="text-4xl font-bold">🌳 Arboleteca</h1>
+          <p className="text-xl mt-2">Descubre los secretos de los árboles que te rodean</p>
+        </header>
+      }
+      {
+        <header className="bg-green-600 text-white text-center p-4 flex flex-row">
+          <h1 className="text-2xl font-bold">🌳 Arboloteca</h1>
+        </header>
 
-
+      }
+    </>
   )
 }
